1a5e1c5560cf4055530b7433fac4c80ce1c02a99
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-04-20 13:29:10 +0100

    removed productPage

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import 'font-awesome/css/font-awesome.min.css';
 import HomePage from "./Pages/HomePage";
-import ProductPage from "./Pages/ProductPage";
 import CartPage from "./Pages/CartPage";
 import Registration from './Pages/Registration'
 import LoginPage from "./Pages/LoginPage"
@@ -17,8 +16,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
       <Routes>
-        <Route path="/" exact element={<HomePage /> }/>
-        <Route path="/product" exact element={<ProductPage /> }/>
+        <Route path="/" exact element={<HomePage /> }/> 
         <Route path="/contact" exact element={<Contact /> }/>
         <Route path="/cartpage" exact element={<CartPage /> }/>
         <Route path="/registration" exact element={<Registration /> }/>
