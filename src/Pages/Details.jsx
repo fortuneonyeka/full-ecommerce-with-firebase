049a2a95065b0cd0b3ca8eed7a513a5488fbf724
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-04-21 20:07:08 +0100

    dynamically display add to cart or remove from cart

--- src/Pages/Details.jsx
+++ src/Pages/Details.jsx
@@ -1,21 +1,69 @@
 import React from 'react'
+import { useParams } from "react-router-dom"
+import { useState } from "react"
+import { useDispatch } from "react-redux"
+import { addItem, delItem } from "../Redux/Actions/actions"
+import { doc, getDoc } from "firebase/firestore";
+import fireDB from "../fireConfig";
+import { fireproducts } from "../products/Fireproducts"
+import { useEffect } from "react"
+
+
+
+
+
 
 const Details = ({productDetails, closeModalProp}) => {
+  // const [product, setProduct] = useState();
+
+  // const proid = useParams();
+
+  // const getData = async () => {
+  //   try {
+  //     const productTemp = await getDoc(doc(fireDB, "products", proid.productid));
+
+  //     setProduct(productTemp);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   getData()
+  // },[])
+
+  const proid = useParams();
+  const proDetail = fireproducts.filter((x)=>x.id === proid.id)
+  const product = proDetail[0]
+  console.log(product);
+  const dispatch = useDispatch()
+
+
+  const [cartBtn, setCartBtn] = useState("Add To Cart")
+
+  const handleCart = (product) => {
+    if (cartBtn === "Add To Cart") {
+      dispatch(addItem(product))
+      setCartBtn("Remove From Cart")
+    }else {
+      dispatch(delItem(product))
+      setCartBtn("Add To Cart")
+    }
+  }
   return (
-    <div className="w-screen  mb-24 fixed flex justify-center items-center bg-gray-400 bg-opacity-30 productDetails">
-      <div className="bg-white p-8 card">
+    <div className="w-screen fixed flex justify-center items-center bg-gray-400 bg-opacity-30 productDetails">
+      <div className="p-8 card">
         <div className="flex justify-center items-center mb-4">
         <button onClick={closeModalProp} className="btn btn-outline-danger mr-4 p-2  w-50  font-bold">Close</button>
-       <button className="btn btn-outline-primary  w-50 p-2">Add To Cart</button>
+       <button onClick={() => handleCart(product)} className="btn btn-outline-primary  w-50 p-2">{cartBtn}</button>
         </div>
       
           <div >
-              <img src={productDetails.imageURL} alt="productDetails" height="700px" width="700px" />
+              <img src={productDetails.imageURL} alt="productDetails" height="600px" width="500px" />
           </div>
-          <div >
-            <hr />
-            <h2 className=" text-base">${productDetails.price}</h2>
-            <h2 className=" text-base">${productDetails.description}</h2>
+          <div className="bg-black text-white rounded p-4">
+            <h2 className=" text-base leading-none">${productDetails.price}</h2>
+            <h2 className=" text-base leading-none break-normal">Desc: {productDetails.description}</h2>
           </div>
          
     </div>
