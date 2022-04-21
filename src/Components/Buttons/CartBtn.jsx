31d5dd2069ef7f79c364db2266af9b5284c71bf5
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-04-21 20:03:23 +0100

    add dynamic cart counter

--- src/Components/Buttons/CartBtn.jsx
+++ src/Components/Buttons/CartBtn.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { useSelector } from "react-redux"
 import { NavLink } from "react-router-dom"
 const CartBtn = () => {
+  const counter = useSelector((state) => state.addItems)
   return (
     <>
-    <NavLink to="/cartpage" className="block lg:inline-block lg:mt-0 text-teal-200 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2  rounded no-underline px-3">
-      <span className="fa fa-shopping-cart me-1"></span> Cart()
+    <NavLink to="/cartpage" className="bg-blue-600 hover:bg-blue-900 text-white font-bold py-2  rounded no-underline px-3">
+      <span className="fa fa-shopping-cart me-1"></span> Cart({counter.length})
     </NavLink>
     </>
   )
