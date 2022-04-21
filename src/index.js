25ac5cc3f70cde066aa693f0cc9c925e78fa91dc
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-04-21 20:09:58 +0100

    import provider and redux store

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
-// import { Provider } from "react-redux";
+import { Provider } from "react-redux";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from "./Redux/Store/store"
 
 ReactDOM.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
